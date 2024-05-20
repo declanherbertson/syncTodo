5fe090c4d7ac4a6d23e6a14ded0d23e86c8539b1
Declan Herbertson <declanherbertson@gmail.com>
2024-05-19 22:30:45 -0700

    build

--- todo/server/build/app.js
+++ todo/server/build/app.js
@@ -3,14 +3,10 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-var path_1 = __importDefault(require("path"));
 var express_1 = __importDefault(require("express"));
 var routes_1 = __importDefault(require("./routes"));
 var app = express_1.default();
 var PORT = process.env.PORT || 4000;
-app.set('views', path_1.default.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-app.use(express_1.default.static(path_1.default.join(__dirname, 'public')));
 routes_1.default(app);
 app.listen(PORT, function () {
     return console.log("server running in http://localhost:" + PORT);
